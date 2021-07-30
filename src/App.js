11bdb781478b11eb0fb90724a705eb900d4f42da
Aneesh Sharma <aneeshsharma0671@gmail.com>
2021-07-30 23:39:03 +0530

    Changed swiper mode to mousewheel

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Home from "./pages/Home";
 import Team from "./pages/Team";
 import {Swiper,SwiperSlide} from 'swiper/react';
 import 'swiper/swiper-bundle.css';
-import SwiperCore, { Navigation, Pagination} from 'swiper';
+import SwiperCore, { Mousewheel, Navigation, Pagination} from 'swiper';
 
-SwiperCore.use([Navigation, Pagination]);
+SwiperCore.use([Mousewheel, Pagination]);
 function App() {
   return (
     <div className="App" style={{backgroundColor:"#f7f7f7"}}>
       <Header/>
-      <Swiper navigation pagination onSwiper={(swipe)=>console.log(swipe)} style={{width:"100%", height:"100%"}} >
+      <Swiper 
+      
+        onSwiper={(swipe)=>console.log(swipe)} 
+        style={{width:"100%", height:"100%"}} >
         <SwiperSlide><Home/></SwiperSlide>
         <SwiperSlide><Team/></SwiperSlide>
       </Swiper>
