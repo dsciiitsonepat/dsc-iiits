c7d997cf9205823dd544d37f355dfe0d4e20c59e
therealansh <tyagiansh23@hotmail.com>
2021-07-30 19:49:18 +0530

    add: backend support(firebase)

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import TeamCard from "../components/TeamCard";
 import "../styles/team.css";
-
+import firebase from '../firebase';
+import { useEffect } from "react";
 
 const COLORS = ["#FFD65F", "#FF4D4D", "#109D58", "#4286F5"]
 export default function Team() {
-	let i = 0;
+	const [members,setMembers]=  useState([])
+	useEffect(() => {
+		const fetchMembers = async () => {
+			const db = firebase.firestore();
+			const data = await db.collection("members").get()
+			const members = data.docs.map(doc => doc.data())
+			setMembers(members)
+		}
+		fetchMembers()
+	},[])
+
   return (
     <div style={{height:"100vh", display:"flex", justifyContent:"center",alignItems:"center", flexDirection:"column"}}>
       <h2>Team</h2>
-	  <TeamCard color={COLORS[i]}/>
+	  <div style={{width:"65%", display:"flex",justifyContent:"space-evenly", flexDirection:"row", height:"75%", alignItems:"center"}}>
+	  {
+		  members.map((member,index) => {
+			  return <TeamCard key={index} member={member} color={COLORS[index%COLORS.length]}/>
+		  })
+	  }
+	  </div>
     </div>
   );
 }
