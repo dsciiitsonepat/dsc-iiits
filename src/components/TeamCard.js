93f45b8197c44ebd00c046229054e68e6e4796d2
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2021-08-07 17:02:46 +0530

    teampage-responsive

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react"
 
 export default function TeamCard(props) {
-	return (
-		<div className="card">
-			<div className="bg-card">
-				<div className="card-header" style={{backgroundColor:props.color}}></div>
-				<div className="card-content" style={{backgroundColor:"#c5c5c5"}}>
-					<h2>{props.member.name}</h2>
-					<p>{props.member.role}</p>
-				</div>
-			</div>
-			<div className="img-circle" style={{backgroundColor:"#f0f0f0"}}></div>
-		</div>
-	)
+  return (
+    <div className="card">
+      <div className="bg-card">
+        <div className="card-header" style={{ backgroundColor: props.color }}></div>
+        <div className="card-content" style={{ backgroundColor: "#c5c5c5" }}>
+          <h2>{props.member.name}</h2>
+          <p>{props.member.role}</p>
+        </div>
+      </div>
+      <div className="img-circle" style={{ backgroundColor: "#f0f0f0" }}></div>
+    </div>
+  )
 }
--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import "../styles/team.css";
-import TeamCarousal from "../components/TeamCarousal";
-
-
+import React from "react"
+import "../styles/team.css"
+import TeamCarousal from "../components/TeamCarousal"
 
 export default function Team() {
-
   return (
     <>
-	 <div className = "team-main">
-	 	<div className = "team-page">
-	 		<h1>Team</h1>
-            <div style={{display:"flex", flexDirection:"row",position:"relative"}}>
-               <TeamCarousal/>
+      <div className="team-main">
+        <div className="team-page">
+          <h1>Team</h1>
+          <div className="teamcarousal-container" style={{ display: "flex", flexDirection: "row", position: "relative" }}>
+            <TeamCarousal />
           </div>
-	 	</div>
-	 </div> 
+        </div>
+      </div>
     </>
-  );
+  )
 }
