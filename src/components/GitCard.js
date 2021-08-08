d8660621d2ac18b4c9bd46bb69da6cba722eb0cb
ShivanshVerma-coder <75882684+ShivanshVerma-coder@users.noreply.github.com>
2021-08-08 22:49:43 +0530

    Project card now responsive (#34)
    * Fixed Logo image change on scroll
    
    * centered image in team card
    
    * Fixed Font size in team card
    
    * project-card-responsive
    
    * project-card-responsive-again
    
    Co-authored-by: Aneesh Sharma <aneeshsharma0671@gmail.com>
    Co-authored-by: Aneesh Sharma <54682356+aneeshsharma0671@users.noreply.github.com>

--- src/components/GitCard.js
+++ src/components/GitCard.js
@@ -1,24 +1,16 @@
-import React, { useEffect, useState } from "react";
-import StackedAvatar from "./StackAvatar";
-import {
-  Card,
-  CardContent,
-  CardMedia,
-  Typography,
-  makeStyles,
-  useTheme,
-} from "@material-ui/core";
+import React, { useEffect, useState } from "react"
+import StackedAvatar from "./StackAvatar"
+import { Card, CardContent, CardMedia, Typography, makeStyles, useTheme } from "@material-ui/core"
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
+    flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
     borderRadius: "10px",
-    height: "25vh",
-    [theme.breakpoints.down("sm")]: {
-      width: "75vw",
-      height: "85vw",
+    minHeight: "25vh",
+    [theme.breakpoints.down("md")]: {
       flexDirection: "column",
     },
   },
@@ -28,9 +20,15 @@ const useStyles = makeStyles((theme) => ({
   },
   content: {
     flex: "1 0 auto",
+    textAlign: "start",
+    width: "80%",
+    [theme.breakpoints.down("md")]: {
+      width: "100%",
+    },
   },
   cover: {
     width: 165,
+    [theme.breakpoints.down("md")]: {},
   },
   stack: {
     display: "flex",
@@ -38,30 +36,28 @@ const useStyles = makeStyles((theme) => ({
     paddingLeft: theme.spacing(1),
     paddingBottom: theme.spacing(1),
   },
-}));
+}))
 
 export default function GitCard({ project }) {
-  const theme = useTheme();
-  const classes = useStyles(theme);
-  const [contributors, setContributors] = useState([]);
+  const theme = useTheme()
+  const classes = useStyles(theme)
+  const [contributors, setContributors] = useState([])
 
   useEffect(() => {
     const getContributors = async () => {
-      const response = await fetch(
-        `https://api.github.com/repos/dsciiitsonepat/${project.name}/contributors`
-      );
-      const data = await response.json();
-      const avatar = [];
+      const response = await fetch(`https://api.github.com/repos/dsciiitsonepat/${project.name}/contributors`)
+      const data = await response.json()
+      const avatar = []
       data.forEach((contributor) => {
         avatar.push({
           githubHandle: contributor.login,
           avatar: contributor.avatar_url,
-        });
-      });
-      setContributors(avatar);
-    };
-    getContributors();
-  }, [project.name]);
+        })
+      })
+      setContributors(avatar)
+    }
+    getContributors()
+  }, [project.name])
 
   return (
     <a href={project.svn_url} style={{ color: "#000", textDecoration: "none" }}>
@@ -76,16 +72,11 @@ export default function GitCard({ project }) {
             </Typography>
           </CardContent>
           <div className={classes.stack}>
-            <StackedAvatar
-              maxAvatars={3}
-              round={true}
-              size={35}
-              avatars={contributors}
-            />
+            <StackedAvatar maxAvatars={3} round={true} size={35} avatars={contributors} />
           </div>
         </div>
         <CardMedia className={classes.cover} image={project.owner.avatar_url} />
       </Card>
     </a>
-  );
+  )
 }
